4baf5163bf08f655ce7b80079df06f99c13f0cb5
Asishpatra34 <asishpatra34@gmail.com>
2023-05-15 19:54:05 +0530

    action created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { StoreModule } from "@ngrx/store";
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { NavbarComponent } from './navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreDevtoolsModule,
     StoreModule.forRoot({})
   ],
   providers: [],
