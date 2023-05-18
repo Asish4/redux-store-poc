3ea229cebaef54bc85e5fb152fdb7ab316a6c218
Asishpatra34 <asishpatra34@gmail.com>
2023-05-18 18:45:49 +0530

    using selector

--- src/app/customers/customer-list/customer-list.component.ts
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -1,14 +1,19 @@
 import { state } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { Store } from "@ngrx/store";
+import { Store, select } from "@ngrx/store";
+
 import * as customerActions from "../state/customer.actions";
+import { Observable } from 'rxjs';
+import * as fromCustomer from "../state/customer.reducer";
+import { Customer } from "../customer.model";
+
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.scss']
 })
 export class CustomerListComponent implements OnInit {
-  customers: any;
+  customers$: Observable<Customer[]> | undefined;
 
   constructor(private store: Store<any>){}
 
@@ -17,7 +22,7 @@ export class CustomerListComponent implements OnInit {
     // this.store.dispatch({type:'LOAD_CUSTOMERS'}) 
     // this.store.subscribe(state=>(this.customers=state.customers.customers))
     this.store.dispatch(new customerActions.LoadCustomers())
-    this.store.subscribe(state=>(this.customers=state.customers.customers))
+    this.customers$ = this.store.pipe(select(fromCustomer.getCustomers));
 
   }
 }
